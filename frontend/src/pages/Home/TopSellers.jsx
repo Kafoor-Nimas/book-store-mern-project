a3e8a93bbd0c8d1c95605e4c32c7aaac609f5f04
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-22 14:16:07 +0530

    swiper js initial setup

--- frontend/src/pages/Home/TopSellers.jsx
+++ frontend/src/pages/Home/TopSellers.jsx
@@ -1,4 +1,17 @@
 import React, { useEffect, useState } from "react";
+import Bookcard from "../books/Bookcard";
+import { useRef } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import './styles.css';
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 const categories = [
   "Choose a genre",
@@ -43,9 +56,36 @@ const TopSellers = () => {
           ))}
         </select>
       </div>
-      {filterdBooks.map((book, index) => (
-        <div>{book.title}</div>
-      ))}
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {filterdBooks.length > 0 &&
+          filterdBooks.map((book, index) => (
+            <SwiperSlide key={index}>
+              <Bookcard book={book} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </div>
   );
 };
