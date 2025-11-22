9cc8b445ba0850e5992e635d13522702572682bd
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-22 10:49:17 +0530

    Add TopSellers component and books data

---
+++ frontend/src/pages/Home/TopSellers.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+const TopSellers = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("books.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+  return <div>TopSellers</div>;
+};
+
+export default TopSellers;
