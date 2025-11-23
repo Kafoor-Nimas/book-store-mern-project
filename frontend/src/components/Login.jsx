a4488bdf0f92df25956ad98f503fe61493c1713c
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-23 06:58:29 +0530

    Enhance Login component with message display and Google sign-in button

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
 
 const Login = () => {
+  const [message, setMessage] = useState("");
   return (
     <div className="h-[calc(100vh-120px)]  flex justify-center items-center">
       <div className="w-full max-w-sm mx-auto bg-white px-8 pt-6 pb-8 mb-4 rounded shadow-md">
@@ -37,6 +39,9 @@ const Login = () => {
               className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
             />
           </div>
+          {message && (
+            <p className="text-red-500 italic text-xs mb-3">{message}</p>
+          )}
           <div>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-8 rounded focus:outline-none py-2">
               Login
@@ -48,6 +53,16 @@ const Login = () => {
           <Link to="/register" className="text-blue-500 hover:text-blue-700">
             Register
           </Link>
+        </p>
+        {/* Google sign in */}
+        <div className="mt-4">
+          <button className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none">
+            <FaGoogle />
+            Sign in with Google
+          </button>
+        </div>
+        <p className="mt-5 text-center text-gray-500 text-xs">
+          &copy;2025 Book Store. All rights reserved.
         </p>
       </div>
     </div>
