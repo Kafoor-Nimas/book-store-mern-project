25b38ae30b56f0ae6434a4627a30de3209cf55fc
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-23 14:46:03 +0530

    Refactor backend setup: integrate mongoose connection and update package.json dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my server!");
-});
+require("dotenv").config();
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+  app.get("/", (req, res) => {
+    res.send("Welcome to my server!");
+  });
+}
+
+main()
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
