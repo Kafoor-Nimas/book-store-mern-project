caa8a84833f3958cbb224285a7a9f2a38870d895
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-24 13:08:14 +0530

    Refactor book route to use postBook controller function

---
+++ backend/src/books/book.controller.js
@@ -0,0 +1,18 @@
+const Book = require("./book.model");
+
+const postBook = async (req, res) => {
+  try {
+    const newBook = await Book({ ...req.body });
+    await newBook.save();
+    res
+      .status(200)
+      .send({ message: "Book posted  successfully", book: newBook });
+  } catch (error) {
+    console.error("Error creating book", error);
+    res.status(500).send({ message: "Failed to create book" });
+  }
+};
+
+module.exports = {
+  postBook,
+};
--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const Book = require("./book.model");
+const { postBook } = require("./book.controller");
 
 const router = express.Router();
 
 //post a book
-router.post("/create-book", async (req, res) => {
-  try {
-    const newBook = await Book({ ...req.body });
-    await newBook.save();
-    res
-      .status(200)
-      .send({ message: "Book posted  successfully", book: newBook });
-  } catch (error) {
-    console.error("Error creating book", error);
-    res.status(500).send({ message: "Failed to create book" });
-  }
-});
+router.post("/create-book", postBook);
+
+// get all books
 
 module.exports = router;
