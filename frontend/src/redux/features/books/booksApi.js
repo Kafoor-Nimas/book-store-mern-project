74359e8b0b51c553f77c6e126e5ea84bc74a965c
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-25 08:18:52 +0530

    Fix typo in providedTags for fetchAllBooks and add missing endpoints for fetching, adding, updating, and deleting books

--- frontend/src/redux/features/books/booksApi.js
+++ frontend/src/redux/features/books/booksApi.js
@@ -20,10 +20,46 @@ const booksApi = createApi({
   endpoints: (builder) => ({
     fetchAllBooks: builder.query({
       query: () => "/",
-      providedTags: ["Books"],
+      providesTags: ["Books"],
+    }),
+    fetchBookById: builder.query({
+      query: (id) => `/${id}`,
+      providesTags: (result, error, id) => [{ type: "Books", id }],
+    }),
+    addBook: builder.mutation({
+      query: (newBook) => ({
+        url: "/create-book",
+        method: "POST",
+        body: newBook,
+      }),
+      invalidatesTags: ["Books"],
+    }),
+    updateBook: builder.mutation({
+      query: ({ id, ...rest }) => ({
+        url: `/edit/${id}`,
+        method: "PUT",
+        body: rest,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+      invalidatesTags: ["Books"],
+    }),
+    deleteBook: builder.mutation({
+      query: (id) => ({
+        url: `/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Books"],
     }),
   }),
 });
 
-export const { useFetchAllBooksQuery } = booksApi;
+export const {
+  useFetchAllBooksQuery,
+  useFetchBookByIdQuery,
+  useAddBookMutation,
+  useUpdateBookMutation,
+  useDeleteBookMutation,
+} = booksApi;
 export default booksApi;
